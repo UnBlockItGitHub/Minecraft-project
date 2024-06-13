0a7859017a60ea355d4643c0420b72e92d075e7c
UnBlockItGitHub <169111520+UnBlockItGitHub@users.noreply.github.com>
2024-06-14 11:36:48 +1200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const Config = {
+    SCREEN_WIDTH: window.innerWidth,
+    SCREEN_HEIGHT: window.innerHeight,
+    BLOCK_SIZE: 1,
+    CHUNK_SIZE: 16,
+    WORLD_HEIGHT: 16,
+    SKY_COLOR: 0x87ceeb,
+};
